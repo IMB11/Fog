66be970a1aa81bd147bb5c2854530b958d7807a5
Calum <contact@mineblock11.dev>
2024-07-27 19:11:10 +0100

    Fix cutoff above clouds.

--- src/main/java/dev/imb11/fog/client/util/math/HazeCalculator.java
+++ src/main/java/dev/imb11/fog/client/util/math/HazeCalculator.java
@@ -4,7 +4,12 @@ import dev.imb11.fog.client.FogManager;
 import dev.imb11.fog.client.registry.FogRegistry;
 import dev.imb11.fog.client.resource.CustomFogDefinition;
 import dev.imb11.fog.client.util.color.Color;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.Heightmap;
 
 public class HazeCalculator {
     private static final int[] times = {
@@ -15,9 +20,13 @@ public class HazeCalculator {
             0.85, 0.25, 0.25, 0.25, 0.85, 0.5, 0.5, 0.85
     };
 
+	private static float timeAboveSurface = 0;
+
     public static FogManager.FogSettings applyHaze(float undergroundFactor, FogManager.FogSettings settings, int timeOfDay) {
 //		return settings;
-
+	    MinecraftClient client = MinecraftClient.getInstance();
+	    ClientWorld world = client.world;
+		ClientPlayerEntity player = client.player;
 		double hazeValue = getHaze(timeOfDay);
 
 		CustomFogDefinition.FogColors belowGroundColors = FogRegistry.getDefaultCaveColors();
@@ -32,6 +41,22 @@ public class HazeCalculator {
 	    fogColorR = MathHelper.lerp(undergroundFactor, belowColor.red / 255f, fogColorR);
 	    fogColorG = MathHelper.lerp(undergroundFactor, belowColor.green / 255f, fogColorG);
 	    fogColorB = MathHelper.lerp(undergroundFactor, belowColor.blue / 255f, fogColorB);
+
+	    int surfaceTopLevel = world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (int) player.getX(), (int) player.getZ());
+	    int viewCutoff = surfaceTopLevel + (client.options.getViewDistance().getValue() * 32);
+	    int distancePastCutoff = (int) (player.getY() - viewCutoff);
+
+		// TODO: Try and do something with fogStart here, eg: when fogStart as a blockpos is above the top level, start lerping.
+	    // Check if player cannot see the top of the surface using their viewDistance option
+	    if (client.player.getY() > viewCutoff) {
+		    // Range from viewCutoff to viewCutoff + 25
+		    float percentageCutoff = MathHelper.clamp(distancePastCutoff / 25f, 0, 1);
+		    Vec3d skyColour = client.world.getSkyColor(player.getPos(), client.getTickDelta());
+		    // Lerp between the fog color and the sky color
+		    fogColorR = (float) MathHelper.lerp(percentageCutoff, fogColorR, skyColour.x);
+		    fogColorG = (float) MathHelper.lerp(percentageCutoff, fogColorG, skyColour.y);
+		    fogColorB = (float) MathHelper.lerp(percentageCutoff, fogColorB, skyColour.z);
+	    }
 
         return new FogManager.FogSettings(settings.fogStart(), settings.fogEnd(), fogColorR, fogColorG, fogColorB);
     }
