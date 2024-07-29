7e664f6c4dc1ae8cbb8aa93345e3d9c61a70e332
Calum <contact@mineblock11.dev>
2024-07-29 21:59:10 +0100

    feat: YACL translations

--- src/main/java/dev/imb11/fog/loaders/FogClientForge.java
+++ src/main/java/dev/imb11/fog/loaders/FogClientForge.java
@@ -2,12 +2,26 @@
 /*package dev.imb11.fog.loaders;
 
 import dev.imb11.fog.client.FogClient;
+import dev.imb11.fog.config.FogConfig;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.BiFunction;
 
 @Mod("fog")
 public class FogClientForge {
 	public FogClientForge() {
 		FogClient.initialize();
+
+		ModLoadingContext.get().registerExtensionPoint(
+				ConfigScreenHandler.ConfigScreenFactory.class,
+				() -> new ConfigScreenHandler.ConfigScreenFactory(
+						(minecraftClient, parent) -> FogConfig.getInstance().getYetAnotherConfigLibInstance().generateScreen(parent)
+				)
+        );
 	}
 }
 *//*?}*/
