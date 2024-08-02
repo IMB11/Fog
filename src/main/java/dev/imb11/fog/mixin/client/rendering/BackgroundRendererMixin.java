0b0b8c8365228a4859476eb7f2681db0921092a4
Calum <contact@mineblock11.dev>
2024-08-02 13:33:27 +0100

    fix: Wrap import

--- src/main/java/dev/imb11/fog/mixin/client/rendering/BackgroundRendererMixin.java
+++ src/main/java/dev/imb11/fog/mixin/client/rendering/BackgroundRendererMixin.java
@@ -15,9 +15,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 
 /*? if >=1.21 {*/
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.block.enums.CameraSubmersionType;
 /*?}*/
 
