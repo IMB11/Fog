b5bbe0aaa061936237855833618de26ff451c8ab
Steveplays28 <djspaargaren@outlook.com>
2024-08-02 17:57:42 +0200

    fix: Fix Sodium compatibility
    The mixin plugin handles Sodium being installed correctly now.

--- src/main/java/dev/imb11/fog/loaders/mixin/FogMixinPlugin.java
+++ src/main/java/dev/imb11/fog/loaders/mixin/FogMixinPlugin.java
@@ -1,33 +1,48 @@
 package dev.imb11.fog.loaders.mixin;
 
-import dev.imb11.fog.client.FogClient;
-import dev.imb11.fog.config.FogConfig;
+import com.google.common.collect.ImmutableMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
+
+import static dev.imb11.fog.client.FogClient.isModInstalled;
 
 public class FogMixinPlugin implements IMixinConfigPlugin {
+	private static final @NotNull Supplier<Boolean> TRUE = () -> true;
+	private static final @NotNull String SODIUM_MOD_ID = "sodium";
+	private static final @NotNull String EMBEDDIUM_MOD_ID = "embeddium";
+	private static final @NotNull Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
+			"dev.imb11.fog.mixin.client.rendering.WorldRendererMixin",
+			() -> !isModInstalled(SODIUM_MOD_ID) && !isModInstalled(EMBEDDIUM_MOD_ID)
+	);
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+	}
 
 	@Override
 	public void onLoad(String mixinPackage) {}
 
 	@Override
-	public String getRefMapperConfig() { return ""; }
-
-	@Override
-	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-		return !FogClient.isModInstalled("sodium")
-				|| !mixinClassName.startsWith("dev.imb11.fog.mixin.client.rendering.WorldRendererMixin");
+	public @Nullable String getRefMapperConfig() {
+		return null;
 	}
 
 	@Override
 	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
 
 	@Override
-	public List<String> getMixins() { return null; }
+	public @Nullable List<String> getMixins() {
+		return null;
+	}
 
 	@Override
 	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
