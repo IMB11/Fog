a5ffc0461444c7f209fa440c6341fe51d0cd1884
Calum <contact@mineblock11.dev>
2024-08-03 21:51:44 +0100

    debug: loggers

--- src/main/java/dev/imb11/fog/mixin/client/resource/ReloadableResourceManagerImplMixin.java
+++ src/main/java/dev/imb11/fog/mixin/client/resource/ReloadableResourceManagerImplMixin.java
@@ -1,24 +1,45 @@
 package dev.imb11.fog.mixin.client.resource;
 
+import dev.imb11.fog.client.FogClient;
 import dev.imb11.fog.client.resource.FogResourcePack;
 import dev.imb11.fog.client.resource.FogResourceUnpacker;
 import net.minecraft.resource.LifecycledResourceManagerImpl;
+import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourcePack;
+import net.minecraft.resource.ResourceType;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(LifecycledResourceManagerImpl.class)
+@Mixin(ReloadableResourceManagerImpl.class)
 public class ReloadableResourceManagerImplMixin {
-	@ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
-	private static @NotNull List<ResourcePack> fog$modifyResourcePackList(List<ResourcePack> packs) {
+	@Shadow
+	@Final
+	private ResourceType type;
+
+	@ModifyArg(
+			method = "reload",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/LifecycledResourceManagerImpl;<init>(Lnet/minecraft/resource/ResourceType;Ljava/util/List;)V"),
+			index = 1
+	)
+	private List<ResourcePack> onPostReload(List<ResourcePack> packs) {
+		FogClient.LOGGER.info("onPostReload: " + this.type);
+		if (this.type != ResourceType.CLIENT_RESOURCES)
+			return packs;
+
+		FogClient.LOGGER.info("Preparing namespace walking.");
 		FogResourceUnpacker.walkNamespaces();
-		return new ArrayList<>(packs) {{
-			add(0, new FogResourcePack());
-		}};
+		FogClient.LOGGER.info("Walked namespaces.");
+		List<ResourcePack> list = new ArrayList<>(packs);
+		list.add(0, new FogResourcePack());
+		FogClient.LOGGER.info("Added Fog Resource Pack to the resource pack list.");
+		return list;
 	}
 }
