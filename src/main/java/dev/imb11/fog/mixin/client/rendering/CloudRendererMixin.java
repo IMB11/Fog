e8073ef96d699b48d782946a634e1299bd907cd1
Steveplays28 <djspaargaren@outlook.com>
2025-04-17 11:52:21 +0200

    fix: Comment out an import for another Minecraft version

--- src/main/java/dev/imb11/fog/mixin/client/rendering/CloudRendererMixin.java
+++ src/main/java/dev/imb11/fog/mixin/client/rendering/CloudRendererMixin.java
@@ -15,10 +15,10 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//? }
+//?}
 //? if >=1.21.2 && <1.21.5 {
-import org.joml.Matrix4f;
-//? }
+/*import org.joml.Matrix4f;
+*///?}
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
